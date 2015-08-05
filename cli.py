dbc347f908dca2ba09e35ff836dc862874cc4cd1
Louis Cruz <cruzlouis13@gmail.com>
2015-08-04 22:30:40 -0700

    setup basic flix commands

--- cli.py
+++ cli.py
@@ -1,20 +1,32 @@
 import click
 from googlemovieshowtimes import GoogleMovieShowtimes
 
-@click.command()
-@click.argument('zipcode', default='', required=False)
-def main(zipcode):
-    """A command line tool to get the info on all the latest movies."""
-    parser = GoogleMovieShowtimes(zipcode,"","")
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('location', default='', nargs=-1, required=False)
+def main(location):
+    """A command line tool to get the info on all the latest movies.
+       ex: flix New
+       """
+    ## locations with more than one word, ie. new york
+    if (len(location) > 1):
+        string = ""
+        for arg in location:
+            string += arg + "+"
+        string = string[:-1]
+    else:
+        string = location
+
+    parser = GoogleMovieShowtimes(string,"","")
     database = parser.parse()
-    ##click.echo(database)
-    ## #click.echo(parser.makeNice())
     
     data = database['theater']
-    theater = data[1]
+    theater = data[0]
+    theater_name = theater['name']
+    click.echo("")
+    click.echo('{0}:'.format(theater_name))
+
     movies = theater['movies']
-    name = theater['name']
-    click.echo('{0}'.format(name))
 
     for titles in movies:
         movie_name = titles['name']
